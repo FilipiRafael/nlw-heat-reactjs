327f32ba62a5d33ac8b825f1f5445d5edf8bdf4d
Filipi Rafael <filipirafael@MacBook-Air-de-Filipi.local>
2022-03-24 23:55:35 -0300

    updates

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,10 +1,12 @@
+import { VscGithubInverted} from "react-icons/vsc"
 import styles from "./styles.module.scss"
 
 export function LoginBox() {
     return (
         <div className={styles.LoginBoxWrapper}>
-        <strong>Entre e ocmpartilhe sua mensagem</strong>
+        <strong>Entre e compartilhe sua mensagem</strong>
         <a href="#" className={styles.signInWithGitHub}>
+            <VscGithubInverted size="24" />
             Entrar com GitHub
         </a>
 
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,44 @@
+import styles from "./styles.module.scss"
+import logoImg from "../../assets/logo.svg";
+
 export function MessageList() {
     return (
-        <h1>MessageList</h1>
+        <div className={styles.MessageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/filipirafael.png" alt="Filipi Rafael" />
+                        </div>
+                        <span>Filipi Rafael</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/filipirafael.png" alt="Filipi Rafael" />
+                        </div>
+                        <span>Filipi Rafael</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/filipirafael.png" alt="Filipi Rafael" />
+                        </div>
+                        <span>Filipi Rafael</span>
+                    </div>
+                </li>
+
+                
+            </ul>
+        </div>
     )
 }
